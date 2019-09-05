1ada96b4c2fc4a9aa45d2a2f236ec898e912f641
lxj <liuxianjunemail@163.com>
2019-09-05 15:34:29 +0800

    init

---
+++ index.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+const inquirer = require('inquirer');
+const program = require("commander");
+const shell = require("shelljs");
+const chalk = require("chalk");
+
+console.log('process.argv', process.argv);
+console.log('process.cwd', process.cwd());
+
+program.version(require("./package").version);
+program.command('init').description('创建项目').action(() => {
+    inquirer
+        .prompt([
+            /* Pass your questions in here */
+            {
+                type: 'input',
+                message: '输入类容',
+                name: 'name'
+            }
+        ])
+        .then(answers => {
+            // Use user feedback for... whatever!!
+            console.log('answers', answers)
+            inquirer
+                .prompt([{
+                    type: 'list',
+                    message: '选择类容',
+                    name: 'name',
+                    choices: [
+                        "Apple",
+                        "Pear",
+                        "Banana"
+                    ]
+                }])
+                .then(answers => {
+                    // Use user feedback for... whatever!!
+                    console.log('answers', answers)
+                });
+        });
+});
+
+program.command('shell').description('shell example').action(() => {
+    const remote = 'https://github.com/pipiyuan/test.git';
+    const projectName = 'test';
+    const command = `
+		git clone ${remote}
+		cd ${projectName}
+		npm install
+	`
+    shell.exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.log(stdout)
+            console.log(stderr)
+        }
+    })
+});
+program.command('png').description('shell png').action(() => {
+    const remote = 'https://github.com/pipiyuan/test.git';
+    const projectName = 'test';
+    const command = "assss"
+	console.log(chalk.blue(command))
+});
+program.parse(process.argv);
---
+++ readme.md
@@ -0,0 +1,16 @@
+# node_command_line_tool
+===========================
+
+> 开发node命令行工具
+
+### 安装
+  npm install 
+
+### 启动项目
+
+  npm link
+
+> npm link的用途可以用于自己本地开发的模块调试过程避免每次都publish npm包，然后安装调试，link后可全局使用 packge.json 中bin 字段下的命令
+
+[参照资料](http://www.sohu.com/a/275486462_495695)
+
